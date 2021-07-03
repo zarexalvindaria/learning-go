3328b2b86512ea493a7d6835110ce0ff4f174fb0
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2021-07-04 02:55:12 +0800

    added strings

---
+++ learn_strings/learn_strings.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	// s1 := "Hello, world!"
+	// s2 := "😋"
+	// s := "Hello, world!"
+	// b := s[0]
+	// b2 := s[4]
+
+	// s := "Hello, world!"
+	// s2 := s[0:5]
+	// s3 := s[7:12]
+	// s4 := s[:5]
+	// s5 := s[7:]
+
+	// s := "Hello, world!"
+	// s2 := s[:6]
+	// s3 := s[7:]
+	// fmt.Println(s, len(s), s2, len(s2), s3, len(s3))
+
+	// s := "Hello, "
+	// var r rune = 127757
+	// s = s + string(r)
+	// fmt.Println(s)
+
+	var vals [3]int
+	vals[0] = 100
+	vals[1] = 9
+	vals[2] = 7
+
+	fmt.Println(vals, vals[0], vals[1], vals[2])
+}
