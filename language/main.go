b7c77cf2c5e9bf2fbd72b350015a7ce2acfebb0f
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2021-07-08 07:25:00 +0800

    used third party package

---
+++ language/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/zarexalvindaria/language/mapper"
+)
+
+func main() {
+	fmt.Println(mapper.Greet("Howdy, what's new?"))
+	fmt.Println(mapper.Greet("Comment allez vous?"))
+	fmt.Println(mapper.Greet("Wie geht es Ihnen?"))
+}
